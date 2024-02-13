9fdce616b5574262af661a52d22101a871884032
PranotiPD <pranotidurugkar1111@gmail.com>
2024-02-13 17:53:39 +0530

    JWT diagram

--- kafka/kafka/src/main/java/com/example/kafka/Controller.java
+++ kafka/kafka/src/main/java/com/example/kafka/Controller.java
@@ -1,2 +1,23 @@
-package com.example.kafka;public class Controller {
+package com.example.kafka;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @Autowired
+    KafkaTemplate<String, String> kafkaTemplate;
+
+    @Value("${my_applications.topic_value}")
+    String topic;
+
+    @PostMapping("/produce")
+    public void createMessages(@RequestParam("msg") String msg){
+        this.kafkaTemplate.send(topic, msg);
+    }
 }
